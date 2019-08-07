893c5b8d8735b6012eb56bc4d300f02799e35712
Philip Krause <philipkrause7@gmail.com>
2019-08-07 16:17:01 -0400

    Fixed prev button/Concat array on fetch/Added Media Queries

--- src/components/ComicList.js
+++ src/components/ComicList.js
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Header from './Header'
 import Comic from './Comic'
 
-
 export default function ComicList() {
-
   const apiKey = 'kPGEGIWaBYEBsdoL9dPH9C6dNI62IrI8V3zPIv3Qcdov5zxIMP'
 
   const [posts, setposts] = useState([])
@@ -12,39 +10,35 @@ export default function ComicList() {
   const [index, setIndex] = useState(0)
   const [number, setNumber] = useState(0)
 
-  const random = () => Math.floor(Math.random() * 400)
+  const stripLength = posts.length - 1
 
-  const fetchHobbes = (offSet) => {
+  const random = () => Math.floor(Math.random() * 600)
+
+  const fetchHobbes = offSet => {
     fetch(
-      `https://api.tumblr.com/v2/blog/calvinandhobbes-daily.tumblr.com/posts/photo?type=photo&offset=${offSet}&page_number=1&api_key=${apiKey}`,
-      { method: 'GET' })
+      `https://api.tumblr.com/v2/blog/calvinandhobbes-daily.tumblr.com/posts/photo?type=photo&offset=${random()}&page_number=1&api_key=${apiKey}`,
+      { method: 'GET' }
+    )
       .then(resp => resp.json())
       .then(data => {
-        setposts(data.response.posts)
-        console.log(data)
-
+        setposts(prev => prev.concat(data.response.posts))
       })
   }
   useEffect(() => fetchHobbes(offSet), [offSet])
 
-
   const nextSlide = () => {
     setIndex(index + 600)
     setNumber(number + 1)
-    console.log(index)
-
-    if (index > 10800) {
-      setIndex(0)
-      setNumber(0)
+    console.log('strip length', stripLength)
+    console.log('number', number)
+    if (number === stripLength) {
       setoffSet(offSet + 20)
-      // fetchHobbes(offSet)
     }
   }
 
-  const prevSlide = () => {
-    if (index < 0) {
-      setIndex(0)
-      setNumber(20)
+  const prevSlide = e => {
+    if (number === 0) {
+      e.disabled = true
     } else {
       setIndex(index - 600)
       setNumber(number - 1)
@@ -53,30 +47,29 @@ export default function ComicList() {
   return (
     <>
       <Header />
-      <section className='container'>
+      <section className="container">
         <section className={`strip-container active-slide-${number}`}>
-          <div className='comic-container'
+          <div
+            className="comic-container"
             style={{
               transform: `translateX(-${index}px)`
-            }}>
+            }}
+          >
             {posts.map((post, i) => {
-              return (
-                <Comic
-                  key={i}
-                  index={i}
-                  post={post}
-                />
-              )
-            })
-            }
+              return <Comic key={i} index={i} post={post} />
+            })}
           </div>
         </section>
       </section>
-      <div className='button-cont'>
-        <button className='prev-button' onClick={prevSlide}>Previous</button>
-        <button className='next-button' onClick={nextSlide}>Next</button>
-      </div>
 
+      <div className="button-cont">
+        <button className="prev-button" onClick={prevSlide}>
+          Previous
+        </button>
+        <button className="next-button" onClick={nextSlide}>
+          Next
+        </button>
+      </div>
     </>
   )
 }
